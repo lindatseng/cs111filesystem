90fbd3fe8dd5ee800cff137c4e0aea2cd7eab9e6
Linda Tseng <lindatseng.0816@gmail.com>
2014-03-06 00:28:05 -0800

    fix typo

--- ospfsmod.c
+++ ospfsmod.c
@@ -1192,7 +1192,7 @@ ospfs_write(struct file *filp, const char __user *buffer, size_t count, loff_t *
 	// Support files opened with the O_APPEND flag.  To detect O_APPEND,
 	// use struct file's f_flags field and the O_APPEND bit.
 	/* EXERCISE: Your code here */
-	if (flip->f_flags &= O_APPEND)
+	if (filp->f_flags &= O_APPEND)
 		*f_pos = oi->oi_size;
 
 	// If the user is writing past the end of the file, change the file's
